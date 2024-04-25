740d1c3008bd982f5aa227cc3ae7c7751c49a4a6
Simon Métais <metais.simon@icloud.com>
2024-04-25 17:29:31 +0200

    sett

--- django_tests/settings.py
+++ django_tests/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
-# STATICFILES_DIRS = [BASE_DIR / 'vol/static']
+# STATICFILES_DIRS = [BASE_DIR / 'django_tests/static']
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
