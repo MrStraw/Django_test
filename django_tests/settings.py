d74c797a51599d8fdecea7b1302226a9573e78d2
Simon Métais <metais.simon@icloud.com>
2024-06-05 10:48:38 +0200

    css base header

--- django_tests/settings.py
+++ django_tests/settings.py
@@ -63,41 +63,28 @@ TEMPLATES = [
 WSGI_APPLICATION = 'django_tests.wsgi.application'
 
 
-if IN_DOCKER:
-    DATABASES = {
-            "default": {
-                "ENGINE": "django.db.backends.postgresql",
-                "NAME": 'db_name:test',
-                "USER": 'db_user:test',
-                "PASSWORD": 'db_password:test',
-                "HOST": 'db_c',
-                # "PORT": 5432,
-            }
-        }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": 'db_name:test',
+        "USER": 'db_user:test',
+        "PASSWORD": 'db_password:test',
+        "HOST": 'db_c',
+        # "PORT": 5432,
+    } if IN_DOCKER else {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', },
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', },
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator', },
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator', },
 ]
 
 
